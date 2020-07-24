83a2f7a60eac42e0fac398abc8056f8c3a28fbbb
Guido Matos <guido.matos.88@gmail.com>
2020-07-24 00:04:43 -0500

    Implementando los clientes y ordenes en microservicios

---
+++ ECommerce.Api.Customers/Providers/CustomersProvider.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Providers
+{
+    public class CustomersProvider: ICustomersProvider
+    {
+        private readonly CustomersDbContext dbContext;
+        private readonly ILogger<CustomersProvider> logger;
+        private readonly IMapper mapper;
+        public CustomersProvider(CustomersDbContext dbContext, ILogger<CustomersProvider> logger, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeddData();
+        }
+
+        private void SeddData()
+        {
+            if (!dbContext.Customers.Any())
+            {
+                dbContext.Customers.Add(new Db.Customer() { Id = 1, Name = "Guido Matos", Address= "Address 1" });
+                dbContext.Customers.Add(new Db.Customer() { Id = 2, Name = "Rebeca Díaz", Address = "Address 2" });
+                dbContext.SaveChanges();
+            }
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Models.Customer> Customers, string ErrorMessage)> GetCustomersAsync()
+        {
+            try
+            {
+                var Customers = await dbContext.Customers.ToListAsync();
+                if (Customers != null && Customers.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Customer>, IEnumerable<Models.Customer>>(Customers);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool IsSuccess, Models.Customer Customer, string errorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                var Customer = await dbContext.Customers.FirstOrDefaultAsync(p => p.Id == id);
+                if (Customer != null)
+                {
+                    var result = mapper.Map<Db.Customer, Models.Customer>(Customer);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
